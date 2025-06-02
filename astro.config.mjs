ce23073dc3977fbe4f8265e42407c03521834499
Ryan Parrish <churchofslidin@gmail.com>
2025-06-02 16:01:15 -0600

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-// import relativeLinks from 'astro-relative-links';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ryanmparrish.com',
   integrations: [
-    react(),
-    // relativeLinks(),
-  ],
+  react(), sitemap()],
 });
